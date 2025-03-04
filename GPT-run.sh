cf48289a69b14268aba75301ff96512776e115e9
tigerlee0225 <yblee94@outlook.com>
2025-03-04 11:38:15 +0900

    Commit CI workflows and deletion of unnecessary files

--- GPT-run.sh
+++
@@ -1,5 +0,0 @@
-# Docker compose image creation
-docker-compose -f docker-compose.gpt.yml build --no-cache
-
-# Docker compose container run
-docker-compose -f docker-compose.gpt.yml up -d
--- GPT-terminate.sh
+++
@@ -1,8 +0,0 @@
-# Docker compose container down
-docker-compose -f docker-compose.gpt.yml down
-
-# Docker compose db remove
-docker-compose -f docker-compose.gpt.yml down --volumes
-
-# Docker compose image remove
-docker-compose -f docker-compose.gpt.yml down --rmi local
